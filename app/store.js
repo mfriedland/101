2b5c8150fdd68a239f6655b020605552877bf079
mfriedland <micahfriedland@gmail.com>
2018-04-12 14:00:50 -0400

    fix: returned store to persist

--- app/store.js
+++ app/store.js
@@ -1,30 +1,23 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import loggingMiddleware from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import loggingMiddleware from 'redux-logger'; // https://github.com/evgenyrodionov/redux-logger
+import thunkMiddleware from 'redux-thunk'; // https://github.com/gaearon/redux-thunk
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/index';
+import { persistStore, persistReducer, autoRehydrate } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // USING ASYNC INSTEAD
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import { AsyncStorage } from 'react-native';
 
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
-)
 
-export const store = createStore(rootReducer, middleware);
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  stateReconciler: autoMergeLevel2 // see "Merge Process" section for details.
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer,   composeWithDevTools(applyMiddleware(thunkMiddleware, loggingMiddleware)));
 
 export default store;
 
-
-
-// import { persistStore, persistReducer, autoRehydrate } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage'; // USING ASYNC INSTEAD
-// import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-// import   { AsyncStorage } from 'react-native';
-
-
-// const reducer = combineReducers({ loading, url, speech, speeches, user })
-
-// const persistConfig = {
-//     key: 'root',
-//     storage: AsyncStorage,
-//     stateReconciler: autoMergeLevel2 // see "Merge Process" section for details.
-// };
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const persistor = persistStore(store);
